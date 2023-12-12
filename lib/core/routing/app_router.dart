923aadb74f79c0c0575d84885b4b83187d2d2440
Abd2lhakam19 <midoahme93@gmail.com>
2023-12-12 17:35:05 +0200

    Finished On Boarding Screen

---
+++ lib/core/routing/app_router.dart
@@ -0,0 +1,22 @@
+import 'package:cooking_app/core/routing/routes.dart';
+import 'package:cooking_app/features/onBoarding/ui/on_boarding_screen.dart';
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  Route generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case Routes.onBoarding:
+        return MaterialPageRoute(
+          builder: (context) => const OnBoardingScreen(),
+        );
+      default:
+        return MaterialPageRoute(
+          builder: (context) => const Scaffold(
+            body: Center(
+              child: Text("Page Not Found"),
+            ),
+          ),
+        );
+    }
+  }
+}
